095a8094fc8d41952ec4493dc20a03f16704427d
krishanu230 <krishanu230@hotmail.com>
2023-04-20 18:21:30 +0530

    f

--- d1.py
+++ d1.py
@@ -1,12 +1,16 @@
+import logging
+
 from django.db.models import Count, Prefetch, Q
 from django_filters.rest_framework import DjangoFilterBackend, FilterSet
 from drf_spectacular.utils import extend_schema
+from knowl_api.core import utils
 from knowl_api.core.authentication import UserAPIKeyAuthentication
-from knowl_api.core.code_integrations.github import (
-    GithubAuthorizationError,
-    GithubIntegration,
+from knowl_api.core.code_integrations import (
+    GitAuthorizationError,
+    git_platform_registry,
 )
 from knowl_api.core.models import (
+    AutomatedDocMetadata,
     Company,
     Document,
     DocumentBranch,
@@ -17,9 +21,13 @@ from knowl_api.core.models import (
     DocumentImage,
     PRDocWorkflow,
     PRNoUpdateReason,
+    UserAPIKey,
+    UserCoachMarkProgress,
 )
 from knowl_api.core.permissions import IsDocumentAuthenticated
 from knowl_api.core.serializers import (
+    AutomatedDocMetadataSerializer,
+    CodeExplanationSerializer,
     CommentSerializer,
     CompanySerializer,
     DocumentBranchDetailSerializer,
@@ -31,15 +39,23 @@ from knowl_api.core.serializers import (
     DocumentGitNodeSearchOutputSerializer,
     DocumentGitNodeSearchSerializer,
     DocumentGitNodeSerializer,
+    DocumentImageCopySerializer,
     DocumentImageSerializer,
     DocumentSerializer,
     DocumentSerializerWithAncestors,
-    GenerateGithubTokenSerializer,
+    GenerateGitTokenSerializer,
     ImageFilePathSerializer,
     PRDocWorkflowSerializer,
     PRNoUpdateReasonSerializer,
+    RefreshGitTokenSerializer,
+    SocialLoginInputSerializer,
+    UserAPIKeySerializer,
+    UserCoachMarkProgressSerializer,
     UserTokenObtainPairSerializer,
+    ValidateInviteSerializer,
 )
+from knowl_api.core.utils import code_text_to_doc_html
+from knowl_api.core.yjs_api import YjsApi
 from rest_framework import filters, mixins, pagination, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -47,6 +63,8 @@ from rest_framework.response import Response
 from rest_framework.settings import api_settings
 from rest_framework_extensions.mixins import NestedViewSetMixin
 from rest_framework_simplejwt.views import TokenObtainPairView
+
+logger = logging.getLogger(__name__)
 
 
 class CompanyViewSet(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
@@ -59,10 +77,23 @@ class CompanyViewSet(mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     def get_queryset(self):
         return super().get_queryset().filter(id=self.request.user.company_id)
 
+    @extend_schema(request=ValidateInviteSerializer)
+    @action(detail=False, methods=["post"], permission_classes=(AllowAny,))
+    def validate_invite(self, request):
+        input_serializer = ValidateInviteSerializer(data=request.data)
+        input_serializer.is_valid(raise_exception=True)
+        company = input_serializer.validated_data["company"]
+        return Response({"name": company.name})
+
 
 class UserObtainTokenPairView(TokenObtainPairView):
     permission_classes = (AllowAny,)
     serializer_class = UserTokenObtainPairSerializer
+
+
+class SocialLoginView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+    serializer_class = SocialLoginInputSerializer
 
 
 class DocumentViewSet(viewsets.ModelViewSet):
@@ -75,7 +106,7 @@ class DocumentViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         user = self.request.user
-        queryset = Document.objects.filter_by_company(user.company).select_related("created_by")
+        queryset = Document.objects.filter_by_user(user).select_related("created_by")
         if self.action in ["recent", "search"]:
             queryset = queryset.exclude(doc_type=Document.DOC_TYPES.branch)
         if self.action == "list":
@@ -129,9 +160,11 @@ class DocumentViewSet(viewsets.ModelViewSet):
         input_serializer = DocumentGitNodeSearchSerializer(data=request.data)
         input_serializer.is_valid(raise_exception=True)
         # TODO: optimize the query by using lines in query itself (ANY(affected_lines) between start_line and end_line)
-        qs = DocumentGitNode.objects.filter(
-            document__company=request.user.company, document__is_archived=False
-        ).select_related("document", "last_updated_by")
+        qs = (
+            DocumentGitNode.objects.filter_by_user(request.user)
+            .filter(document__is_archived=False)
+            .select_related("document", "last_updated_by")
+        )
         data = input_serializer.validated_data
         if not data:
             qs = qs.none()
@@ -176,20 +209,98 @@ class DocumentViewSet(viewsets.ModelViewSet):
     def search(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
 
+    @extend_schema(request=CodeExplanationSerializer)
+    @action(detail=True, methods=["post"], url_path="generate-code-explanation")
+    def generate_code_explanation(self, request, *args, **kwargs):
+        # TODO: add user/document level rate limit to avoid misuse
+        serializer = CodeExplanationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        html_output = code_text_to_doc_html(serializer.validated_data["code_text"])
+        return Response({"doc_html": html_output})
+
+    @action(detail=True, methods=["post"], url_path="pr-branch")
+    def pr_branch(self, request, **kwargs):
+        document = self.get_object()
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        if document.doc_type == Document.DOC_TYPES.branch:
+            return Response({"id": document.id})
+        branch_data = serializer.validated_data["branch"]
+        document_branches = (
+            DocumentBranch.objects.select_related("document")
+            .filter(
+                document__parent=document,
+                document__doc_type=Document.DOC_TYPES.branch,
+                branch_status=DocumentBranch.BRANCH_STATUS.created,
+                git_owner=branch_data["git_owner"],
+                git_repo=branch_data["git_repo"],
+                git_branch=branch_data["git_branch"],
+            )
+            .prefetch_related("document")
+        )
+        document_branch = document_branches.first()
+        if document_branch:
+            branch = document_branch.document
+        else:
+            data = {
+                "name": branch_data["git_branch"],
+                "branch": {
+                    "branch_name": branch_data["git_branch"],
+                    "git_owner": branch_data["git_owner"],
+                    "git_repo": branch_data["git_repo"],
+                    "git_branch": branch_data["git_branch"],
+                },
+            }
+            yjsapi = YjsApi(token=request.auth.token.decode())
+            response = yjsapi.create_branch(document_id=document.id, data=data)
+            branch_id = response.json()["id"]
+            branch = Document.objects.get(id=branch_id)
+        new_git_nodes = serializer.validated_data.get("new_git_nodes", [])
+        update_git_nodes = serializer.validated_data.get("update_git_nodes", [])
+        if update_git_nodes:
+            not_found = DocumentGitNode.update_live_code(update_git_nodes, branch)
+            new_git_nodes.extend(not_found)
+        if new_git_nodes:
+            utils.update_doc_content(branch, request.auth.token, ai_prompt=None, new_git_nodes=new_git_nodes)
+        return Response({"id": branch.id})
+
 
 class UserViewSet(viewsets.GenericViewSet):
     permission_classes = (IsAuthenticated,)
 
-    @extend_schema(request=GenerateGithubTokenSerializer)
-    @action(detail=False, methods=["post"], url_path="generate-github-token")
-    def generate_github_token(self, request):
+    @extend_schema(request=GenerateGitTokenSerializer)
+    @action(detail=False, methods=["post"], url_path="generate-git-token")
+    def generate_git_token(self, request):
         user = request.user
-        serializer = GenerateGithubTokenSerializer(data=request.data)
+        serializer = GenerateGitTokenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        validated_data = serializer.validated_data
+        git_platform = git_platform_registry[user.company.git_platform]
         try:
-            access_token = GithubIntegration.generate_access_token(serializer.validated_data["code"])
-        except GithubAuthorizationError as e:
+            access_token_dict = git_platform.generate_access_token(
+                validated_data["code"], validated_data["redirect_uri"]
+            )
+        except GitAuthorizationError as e:
+            logger.error("Error in getting access token: %s", str(e))
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        access_token = access_token_dict["access_token"]
+        refresh_token = access_token_dict.get("refresh_token")
+        user.github_token = access_token
+        user.save()
+        return Response({"access_token": access_token, "refresh_token": refresh_token})
+
+    @extend_schema(request=RefreshGitTokenSerializer)
+    @action(detail=False, methods=["post"], url_path="refresh-git-token")
+    def refresh_git_token(self, request):
+        user = request.user
+        serializer = RefreshGitTokenSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        git_platform = git_platform_registry[user.company.git_platform]
+        try:
+            access_token_dict = git_platform.get_token_from_refresh_token(serializer.validated_data["refresh_token"])
+        except GitAuthorizationError as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        access_token = access_token_dict["access_token"]
         user.github_token = access_token
         user.save()
         return Response({"access_token": access_token})
@@ -219,6 +330,7 @@ class DocumentNestedViewSetCreateMixin(NestedViewSetCreateMixin):
             super()
             .get_queryset()
             .filter(document__company_id=self.request.user.company_id, document__is_archived=False)
+            .filter(Q(document__user__isnull=True) | Q(document__user_id=self.request.user.id))
         )
 
 
@@ -235,8 +347,9 @@ class DocumentGitNodeViewSet(DocumentNestedViewSetCreateMixin, viewsets.ModelVie
         instance = self.get_object()
         serializer = DocumentGitNodeCopySerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        destination_doc = serializer.validated_data["destination_document"]
         # TODO: verify if copied instance document has permission.
-        copied_instance = instance.copy(serializer.validated_data["destination_document"])
+        copied_instance = instance.copy(destination_doc)
         return Response(DocumentGitNodeSerializer(copied_instance).data)
 
 
@@ -246,6 +359,17 @@ class DocumentImageViewSet(NestedViewSetCreateMixin, viewsets.ModelViewSet):
     queryset = DocumentImage.objects.all()
     serializer_class = DocumentImageSerializer
     permission_classes = (IsDocumentAuthenticated,)
+
+    @extend_schema(request=DocumentImageCopySerializer)
+    @action(detail=True, methods=["post"])
+    def copy(self, request, parent_lookup_document=None, pk=None):
+        instance = self.get_object()
+        serializer = DocumentImageCopySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        destination_doc = serializer.validated_data["destination_document"]
+        # TODO: verify if copied instance document has permission.
+        copied_instance = instance.copy(destination_doc)
+        return Response(DocumentImageSerializer(copied_instance).data)
 
 
 class DocumentContributorViewSet(NestedViewSetMixin, viewsets.ReadOnlyModelViewSet):
@@ -287,33 +411,8 @@ class CommentViewSet(DocumentNestedViewSetCreateMixin, viewsets.ModelViewSet):
         )
         return qs
 
-    # TODO: need clarification
-    # @action(detail=False, methods=["get"])
-    # def reply_count(self, request, parent_lookup_document=None):
-    #     status_counts = self.get_queryset().values("status").annotate(count=Count("status"))
-    #     result = {status_dict["status"]: status_dict["count"] for status_dict in status_counts}
-    #     return Response(result)
 
-
-class PRNoUpdateReasonViewSet(viewsets.ModelViewSet):
-    """
-    CRUD PR no update reason
-    TODO: Think of a way to make it more secure (may be we can filter by company of last updated by)
-    """
-
-    queryset = PRNoUpdateReason.objects.all().select_related("last_updated_by")
-    serializer_class = PRNoUpdateReasonSerializer
-    permission_classes = (IsAuthenticated,)
-
-    def get_queryset(self):
-        return super().get_queryset().filter(company_id=self.request.user.company_id)
-
-
-class PRDocWorkflowFilter(FilterSet):
-    class Meta:
-        model = PRDocWorkflow
-        fields = ["owner", "repo", "pr_no"]
-
+class RequiredFilterSetMixin(FilterSet):
     def filter_queryset(self, queryset):
         # making sure all fields are required.
         if not all(self.form.cleaned_data.values()):
@@ -321,10 +420,43 @@ class PRDocWorkflowFilter(FilterSet):
         return super().filter_queryset(queryset)
 
 
-class PRDocWorkflowViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+class PRNoUpdateReasonFilter(RequiredFilterSetMixin):
+    class Meta:
+        model = PRNoUpdateReason
+        fields = ["owner", "repo", "pr_no"]
+
+
+class PRDocWorkflowFilter(RequiredFilterSetMixin):
+    class Meta:
+        model = PRDocWorkflow
+        fields = ["owner", "repo", "pr_no"]
+
+
+class AutomatedDocMetadataFilter(RequiredFilterSetMixin):
+    class Meta:
+        model = AutomatedDocMetadata
+        fields = ["owner", "repo"]
+
+
+class PRNoUpdateReasonViewSet(viewsets.ModelViewSet):
     """
     CRUD PR no update reason
-    TODO: Think of a way to make it more secure (may be we can filter by company of last updated by)
+    """
+
+    queryset = PRNoUpdateReason.objects.all().select_related("last_updated_by")
+    serializer_class = PRNoUpdateReasonSerializer
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = api_settings.DEFAULT_AUTHENTICATION_CLASSES + [UserAPIKeyAuthentication]
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = PRNoUpdateReasonFilter
+
+    def get_queryset(self):
+        return super().get_queryset().filter(company_id=self.request.user.company_id).order_by("-modified")
+
+
+class PRDocWorkflowViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
+    """
+    CRUD PR doc workflow run viewset - to store workflow run id of each PR to be used in PR diff page.
     """
 
     queryset = PRDocWorkflow.objects.all().select_related("last_updated_by")
@@ -336,3 +468,53 @@ class PRDocWorkflowViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixin
 
     def get_queryset(self):
         return super().get_queryset().filter(company_id=self.request.user.company_id)
+
+
+class AutomatedDocMetadataViewSet(viewsets.ModelViewSet):
+    """
+    CRUD automated doc metadata viewset - to store metadata and doc and code (file) mapping
+    """
+
+    queryset = AutomatedDocMetadata.objects.all().select_related("last_updated_by")
+    serializer_class = AutomatedDocMetadataSerializer
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = api_settings.DEFAULT_AUTHENTICATION_CLASSES + [UserAPIKeyAuthentication]
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = AutomatedDocMetadataFilter
+
+    def get_queryset(self):
+        return (
+            super()
+            .get_queryset()
+            .filter_by_user(self.request.user)
+            .filter(document__is_archived=False)
+            .select_related("document")
+        )
+
+
+class UserAPIKeyViewSet(viewsets.ModelViewSet):
+    """
+    CRUD User Api Key viewset - to view, generate api key
+    """
+
+    queryset = UserAPIKey.objects.all().order_by("-created")
+    serializer_class = UserAPIKeySerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
+
+
+class UserCoachMarkProgressViewSet(viewsets.ModelViewSet):
+    """
+    CRUD User Coach Mark progress viewset - to maintain coach mark progress at user and story level
+    """
+
+    queryset = UserCoachMarkProgress.objects.all().order_by("-created")
+    serializer_class = UserCoachMarkProgressSerializer
+    permission_classes = (IsAuthenticated,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ("story",)
+
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
